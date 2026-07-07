754be6b63406c2bd43dbf0f7cd72c3c363226dff
Braimah Abiola <braimahabiola5@gmail.com>
2026-07-07 16:15:44 +0000

    Cleanup and install core packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  images: {
+    qualities: [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+  }
 };
 
 export default nextConfig;
